9ea038c658928a59a2880381400245f7190e5471
matcho3 <matcho3@kohama-no-MacBook-Air.local>
2014-11-26 17:45:58 +0900

    modified Login n Out

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,9 @@
 class SearchController < ApplicationController
   def index
   	 @search_word = params[:word]
-    @departures = Trip.where(['departure LIKE ?', "%#{@search_word}%"]).paginate(page: params[:page])
+    @trips = Trip.where(['departure LIKE ?', "%#{@search_word}%"]).paginate(page: params[:page])
     render 'search/index'
+
+
   end
 end
