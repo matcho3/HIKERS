1024329a6d8815024fae75dbd3b9e23cd5cbffa1
Shunsuke Kyuwa <s.kyuwa9@gmail.com>
2014-11-30 12:34:01 +0900

    trip changed

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,8 @@
 class Trip < ActiveRecord::Base
 	belongs_to :driver
-	#validates :departure
+	validates :departure, presence: true #地名じゃなきゃダメ 
+	validates :destination, presence: true #地名じゃなきゃだめ
+	validates :leaving_time, presence: true #日時じゃなきゃダメ
+	validates :capacity, presence: true #数字じゃなきゃダメ
+	validates :price, presence: true #数字じゃなきゃだめ
 end
