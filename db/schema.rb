d622da8ef3a9161e25b1de383fa95be39b1979c1
Shunsuke Kyuwa <s.kyuwa9@gmail.com>
2014-12-08 12:47:30 +0900

    date of issue is added on driver's entry form

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20141207082031) do
-
 
   create_table "books", force: true do |t|
     t.integer  "trip_id"
